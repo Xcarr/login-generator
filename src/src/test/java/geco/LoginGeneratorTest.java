e550c9d0477bf6d47391f2ba2c1b7df6a7a74ab8
Xcarr <xavier.carrau@univ-tlse3.fr>
2019-11-19 09:10:02 +0100

    create test class

---
+++ src/src/test/java/geco/LoginGeneratorTest.java
@@ -0,0 +1,18 @@
+package geco;
+
+public class LoginGenerator {
+
+    private LoginService loginService;
+
+    @Test
+    public LoginGenerator() {
+    }
+
+    @Test
+    public String generateLoginForNomAndPrenom() {
+    }
+
+    @Test
+    private String deAccent(String str) {
+    }
+}
---
+++ src/src/test/java/geco/LoginServiceTest.java
@@ -0,0 +1,26 @@
+package geco;
+
+public class LoginService {
+
+    private ArrayList<String> loginsExistants;
+
+    @Test
+    public LoginService(String[] loginsExistants) {
+    }
+
+    @Test
+    public boolean loginExists(String login) {
+    }
+
+    @Test
+    public void addLogin(String login) {
+    }
+
+    @Test
+    public List<String> findAllLoginsStartingWith(String pref) {
+    }
+
+    @Test
+    public List<String> findAllLogins() {
+    }
+}
