208cd7bb4c2c32745d6790c519298cbe84820fc1
Anthony BORDEAU <Vous@exemple.com>
2019-11-20 16:27:56 +0100

    close #1 fix tests Service

--- src/test/java/geco/LoginGeneratorTest.java
+++ src/test/java/geco/LoginGeneratorTest.java
@@ -8,9 +8,13 @@ import static org.junit.Assert.*;
 
 public class LoginGeneratorTest {
 
+    LoginService loginService;
+    LoginGenerator generator;
 
     @Before
     public void setUp() throws Exception {
+        loginService = new LoginService(new String[] {"JROL",
+                "BPER", "CGUR", "JDU", "JRAL", "JRAL1"});
     }
 
     @Test
